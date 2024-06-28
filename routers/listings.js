7973e9b9aaf30f033a44d45ffa13cd3dc5c64da0
DarshanGTHB <darshanvekariya25102004@gmail.com>
2024-06-28 21:25:17 +0530

    owners view

--- routers/listings.js
+++ routers/listings.js
@@ -16,10 +16,10 @@ router
     .get("/",wrapAsync(listingController.getListings))
     .get("/listings", wrapAsync(listingController.getListings)) //Index Route
     .get("/listings/catagory",wrapAsync(listingController.getRandom))
-    .get("/listings/new",isLoggedIn,listingController.getNew) //New Route
-    .post("/listings",upload.single('listing[image]'),validateListing,wrapAsync(listingController.createListing)) //Create Route
+    .get("/listings/new",listingController.getNew) //New Route
+    .post("/listings",isLoggedIn,upload.single('listing[image]'),validateListing,wrapAsync(listingController.createListing)) //Create Route
     .get("/listings/:id", wrapAsync(listingController.showLising)) //Show Route
-    .get("/listings/:id/edit", isLoggedIn,isOwener ,wrapAsync(listingController.getEditListing)) //Edit Route
+    .get("/listings/:id/edit" ,wrapAsync(listingController.getEditListing)) //Edit Route
     .put("/listings/:id", isLoggedIn, isOwener, upload.single('listing[image]'),validateListing,wrapAsync(listingController.updateListing)) //Update Route
     .delete("/listings/:id", isLoggedIn,isOwener ,wrapAsync(listingController.deleteListing)) //Delete Route
     .get("/data/searchbar",wrapAsync(listingController.getDataForSearchBar))
