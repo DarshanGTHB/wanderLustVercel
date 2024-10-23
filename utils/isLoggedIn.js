5d28a3b79c7839a192be53a71af145262862dd97
DarshanGTHB <153267379+DarshanGTHB@users.noreply.github.com>
2024-10-23 21:05:12 +0530

    Update isLoggedIn.js

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -2,7 +2,7 @@ module.exports = isLoggedIn = (req,res,next) => {
     // console.log(req.originalUrl);
     req.session.redirectUrl = req.originalUrl;
     if(!req.isAuthenticated()){
-        req.flash("error","you'r not logged in.");
+        req.flash("error","you'er not logged in.");
         return res.redirect("/login");
     }
     next();
